dbeb9fcc9d8db219a41610612abbc0d17aa64a0c
IsaiasArce <isaiasarcerodriguez@gmail.com>
2022-08-23 17:03:53 -0600

    Update BookMutation.java

--- CodeBase/src/main/java/com/cenfotec/examen/mutation/BookMutation.java
+++ CodeBase/src/main/java/com/cenfotec/examen/mutation/BookMutation.java
@@ -6,6 +6,8 @@ import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class BookMutation implements GraphQLMutationResolver {
     @Autowired
@@ -13,5 +15,21 @@ public class BookMutation implements GraphQLMutationResolver {
     public Book createBook(String name){
         return this.bookService.createBook(name);
     }
+    public Book updateBook(Integer id, String name, Integer status){
+        Optional<Book> book = bookService.findById(id);
+        if(book.isPresent()){
+            Book bookToUpdate= new Book();
+            bookToUpdate.setStatus(status);
+            bookToUpdate.setName(name);
 
+            bookService.updateBook(bookToUpdate);
+            return  bookToUpdate;
+        }
+        else {
+            return  null;
+        }
+    }
+    public Optional<Book> getBook(int id){
+        return this.bookService.findById(id);
+    }
 }
